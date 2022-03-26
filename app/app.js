b60b08b33b221bcfe2e975d423fcc6043eec3430
smweddell <sweddell@gmx.com>
2022-03-26 17:00:55 -0400

    Setup About Info

---
+++ app/app.js
@@ -0,0 +1,15 @@
+// FAQ Accordion for the About Us Page
+var acc = document.getElementsByClassName("accordion-title");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
